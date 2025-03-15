c176367ab5952d9e8e4040c131855fc29be34b3d
Nicolas Kösling <koslingnicolas@gmail.com>
2025-03-15 17:13:14 +0100

    first commit

---
+++ src/utils/mail.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+import { SMTP_HOST, SMTP_USER, SMTP_PASS } from "../utils/envs";
+import { mailData } from "../types/mailTypes";
+import { htmlContent } from "./mailMessage";
+
+let testAccount = await nodemailer.createTestAccount();
+const testHost = "smtp.ethereal.email";
+
+const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: 587,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASS,
+    },
+});
+
+export const sendEmail = async (data: mailData) => {
+    console.log(SMTP_HOST, SMTP_PASS, SMTP_USER);
+    try {
+        const info = await transporter.sendMail({
+            from: data.from_email,
+            to: "koslingnicolas@gmail.com",
+            subject: "Registratie",
+            html: htmlContent(data),
+        });
+        console.log(info);
+        console.log("Mail verstuurd");
+        console.log(nodemailer.getTestMessageUrl(info));
+    } catch (error) {
+        console.log(error);
+    }
+};
